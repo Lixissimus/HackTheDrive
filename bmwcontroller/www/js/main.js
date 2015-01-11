d4880ac9cc403d10e3e194fd61ddb0d6e70a8d41
Lixissimus <felix.wolff@student.hpi.de>
2015-01-11 03:32:09 -0800

    register at chargepoint

--- bmwcontroller/www/js/main.js
+++ bmwcontroller/www/js/main.js
@@ -1,6 +1,7 @@
-function onAuthenticated(bmwClient) {
-	// var gameController = new GameController(bmwClient);
+var username;
 
+function onAuthenticated(bmwClient, name) {
+	username = name;
 	bmwClient.get(bmwClient.model('Vehicle'), {}, function(error, result) {
 		if (error) {
 			console.log('Error retrieving vehicle information');
@@ -34,5 +35,24 @@ function onAuthenticated(bmwClient) {
 }
 
 function onChargingDetected() {
-	window.location.replace("/hxgl/index.html" + window.location.hash);
+	var socket = new WebSocket('ws://104.131.62.171:8001');
+
+	socket.onopen = function() {
+		console.log('Socket open');
+		var message = JSON.stringify({
+			type: 'register-user',
+			username: username,
+			chargepoint: '0000:017A'
+		});
+		console.log('Sending register-user for %s', username);
+		socket.send(message);
+		socket.close();
+
+		
+	};
+	socket.onclose = function() {
+		console.log('Socket closed');
+		window.location.replace("/hxgl/index.html" + window.location.hash);
+	};
+	socket.onerror = function(err) { console.log('Error in register-user socket'); };
 }
