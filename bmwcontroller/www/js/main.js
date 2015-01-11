c069a364900676e4a0ae796c0c3b40866164fbfa
Lixissimus <felix.wolff@student.hpi.de>
2015-01-10 21:49:11 -0800

    Charging event

--- bmwcontroller/www/js/main.js
+++ bmwcontroller/www/js/main.js
@@ -1,3 +1,38 @@
 function onAuthenticated(bmwClient) {
-	var gameController = new GameController(bmwClient);
+	// var gameController = new GameController(bmwClient);
+
+	bmwClient.get(bmwClient.model('Vehicle'), {}, function(error, result) {
+		if (error) {
+			console.log('Error retrieving vehicle information');
+			return;
+		}
+
+		var Vehicles = bmwClient.getResults(bmwClient.model('Vehicle'), result);
+
+    	// just get the first car
+		var vehicle = Vehicles[0];
+		var observer;
+
+		bmwClient.observe(vehicle, null, function(entity) {
+			if (entity.ChargingStatus === 'Charging') {
+				console.log('Detected charging');
+				bmwClient.unobserve(observer, vehicle, null, null, function(error, result) {
+					if (error) {
+						console.log('Error while unobserving');
+						return;
+					}
+
+					console.log('Successfully unobserved');
+				});
+				onChargingDetected();
+			}
+		}, function(error, result) {
+			console.log('Observer registered');
+			observer = result;
+		});
+	});
+}
+
+function onChargingDetected() {
+	alert('Charging');
 }
